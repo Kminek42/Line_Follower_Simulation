b3205ae08650de335923f4c60862da670e2ecf22
Kminek42 <k.kminkowski@icloud.com>
2023-11-29 23:36:38 +0100

    bug fixes

--- learn.py
+++ learn.py
@@ -4,59 +4,61 @@ import robot
 from time import time
 
 class Network:
-    def __init__(self, w1, w2, b1, b2):
-        if w1.shape[1] != b1.shape[0]:
-            print(f"Error, w1 shape: {w1.shape[1]}, b1 shape: {b1.shape[0]}")
-            exit()
+    def __init__(self, weights: np.array, shape):
+        self.last = np.zeros([8])
+        self.w = []
+        self.genotype = weights
+        self.shape = shape
 
-        
-        if w2.shape[0] != b1.shape[0]:
-            print(f"Error, w2 shape: {w2.shape[0]}, b1 shape: {b1.shape[0]}")
-            exit()
+        # assert shape[0] == shape[-1] + 6, f"Inputs: {shape[0]}, Outputs + 6: {shape[-1] + 6}"
 
-        if w2.shape[1] != b2.shape[0]:
-            print(f"Error, w2 shape: {w2.shape[1]}, b1 shape: {b2.shape[0]}")
-            exit()
-        
-        if w1.shape[0] != b2.shape[0] + 6:
-            print(f"Error, w1 shape: {w1.shape[0]}, b2 shape: {b2.shape[0]} [difference should be 6]")
-            exit()
-        
-        self.w1 = w1
-        self.w2 = w2
-        self.b1 = b1
-        self.b2 = b2
+        parameters_n = 0
+        for i in range(len(shape) - 1):
+            parameters_n += shape[i] * shape[i+1]
+            parameters_n += shape[i+1]
 
-        mem_N = b2.shape[0] - 2
-        self.mem = np.zeros(mem_N)
+        assert len(self.genotype) == parameters_n, f"genotype: {len(self.genotype)}, parameters: {parameters_n}"
+            
+        
+        for i in range(len(shape) - 1):
+            self.w.append(weights[:shape[i] * shape[i+1]].reshape(shape[i], shape[i+1]))
+            weights = weights[shape[i] * shape[i+1]:]
+
+            self.w.append(weights[:shape[i+1]])
+            weights = weights[shape[i+1]:]
 
     def forward(self, inputs):
-        inputs = np.concatenate((inputs, self.mem))
-        inputs = inputs @ self.w1
-        inputs = inputs + self.b1
-        inputs = np.arctan(inputs)
+        if max(inputs) > 0.25:
+            self.last = inputs
+        else:
+            inputs = self.last
 
-        inputs = inputs @ self.w2
-        inputs = inputs + self.b2
-        inputs = np.arctan(inputs)
-
-        self.mem = inputs[2:]
+        output_r = inputs
+        for i in range(0, len(self.w), 2):
+            output_r = output_r @ self.w[i]
+            output_r += self.w[i + 1]
+            output_r = np.arctan(output_r)
         
-        inputs = inputs[:2]
-        return inputs[0], inputs[1]
+        output_r = output_r[0]
 
+        output_l = inputs[::-1]
+        for i in range(0, len(self.w), 2):
+            output_l = output_l @ self.w[i]
+            output_l += self.w[i + 1]
+            output_l = np.arctan(output_l)
+
+        output_l = output_l[0]
+
+        return output_l, output_r
+    
+    def __repr__(self):
+        return f"genotype: {self.genotype}\nshape: {self.shape}"
 
 class Problem:
     def __init__(self):
         self.track = Track()
         self.track.add_segment("straight")
-        self.track.add_segment("90_left")
-        self.track.add_segment("straight")
-        self.track.add_segment("x-intersection")
-        self.track.add_segment("straight")
-        self.track.add_segment("90_right")
-        self.track.add_segment("straight")
-        for i in range(35):
+        for i in range(40):
             self.track.add_segment("")
 
     def get_score(self, network: Network):
@@ -67,7 +69,7 @@ class Problem:
             inputs = np.array(self.robot.get_sensors(self.track))
             e1, e2 = network.forward(inputs)
             s += self.robot.move(e1, e2, 1/100)
-            if self.track.distance_to_chain(self.robot.position[0], self.robot.position[1]) > 0.09:
+            if self.track.distance_to_chain(self.robot.position[0], self.robot.position[1]) > 0.19:
                 return -np.inf
 
             if self.robot.rotation > 170 or self.robot.rotation < -170:
@@ -75,7 +77,12 @@ class Problem:
 
 
         return s
-        
+
+def save_genotype(net: Network, filename):
+    file = open(filename, "w")
+    file.write(f"genotype = np.{repr(net.genotype)}\n\n")
+    file.close()
+
 def swap_elements(arr):
     num_swaps = np.random.randint(0, 3)  # Number of swaps
     for _ in range(num_swaps):
@@ -84,28 +91,65 @@ def swap_elements(arr):
 
     return arr
 
-l = True
+def crossover(gens1, gens2):
+    if len(gens1) != len(gens2):
+        raise ValueError("Arrays must be of the same length")
+
+    crossover_point = np.random.randint(1, len(gens1) - 1)
+
+    if np.random.randint(0, 2) == 0:
+        genotype = np.concatenate((gens1[:crossover_point], gens2[crossover_point:]))
+    
+    else:
+        genotype = np.concatenate((gens2[:crossover_point], gens1[crossover_point:]))
+
+    return genotype
+
+def mutate(genotype, mutation_rate):
+    mask = np.random.rand(len(genotype)) < mutation_rate
+    genotype[mask] = np.random.randn(np.count_nonzero(mask))
+    return genotype
+
+def learn_generation(parent1: Network, parent2: Network, children_n: int, p: Problem, mutation_rate=1e-2):
+    children = [parent1, parent2]
+    top = [0, parent1, 0, parent2]
+
+    for i in range(children_n - 2):
+        new_gens = crossover(parent1.genotype, parent2.genotype)
+        mutate(new_gens, mutation_rate)
+        children.append(Network(new_gens, parent1.shape))
+
+    for child in children:
+        score = p.get_score(child)
+
+        worst_top = 2
+        if top[0] < top[2]:
+            worst_top = 0
+
+        if score > top[worst_top]:
+            top[worst_top] = score
+            top[worst_top + 1] = child
+
+    return top
+
+
+l = False
 
 if l:
-    best_score = 0
+    net1 = Network(np.random.randn(9), [8, 1])
+    net2 = Network(np.random.randn(9), [8, 1])
 
+    p = Problem()
+    s1, p1, s2, p2 = learn_generation(net1, net2, 64, p)
+
+    i = 0
     while 2137:
-        w1 = np.random.randn(9, 4)
-        w2 = np.random.randn(4, 3)
-        b1 = np.random.randn(4)
-        b2 = np.random.randn(3)
+        i += 1
+        s1, p1, s2, p2 = learn_generation(p1, p2, 64, p, 1e-1)
+        print(f"Generation: {i}\nScore: {s1}, {s2}\n\n")
+        if s1 > s2:
+            save_genotype(p1, "genotype.txt")
 
-        net = Network(w1, w2, b1, b2)
-        p = Problem()
-        score = p.get_score(net)
-        
-        if score > best_score:
-            print(score)
-            best_score = score
+        else:
+            save_genotype(p2, "genotype.txt")
 
-            file = open("model.txt", "w")
-            file.write(f"w1 = np.{repr(w1)}\n\n")
-            file.write(f"w2 = np.{repr(w2)}\n\n")
-            file.write(f"b1 = np.{repr(b1)}\n\n")
-            file.write(f"b2 = np.{repr(b2)}\n\n")
-            file.close()
--- main.py
+++ main.py
@@ -8,47 +8,24 @@ from learn import Network
 
 pygame.init()
 
-width, height = 800, 600
+width, height = 1024, 768
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Open Polygon')
 
-ge = GraphicEngine(screen, 800, 600)
+ge = GraphicEngine(screen, 1024, 768)
 ge.camera_FOV = 6
 ge.camera_pos = [0, 1]
 
-
-w1 = np.array([[ 0.0369184 ,  0.10525324, -0.07193377, -0.48344666],
-       [ 2.23112292,  0.53097061, -0.32521209,  0.2320492 ],
-       [ 0.71662317,  2.04525242,  0.26745555,  0.98377448],
-       [ 0.55155775,  0.01668726, -0.0327905 ,  2.24457364],
-       [ 0.70578746, -0.55612778,  2.24594008, -1.6007702 ],
-       [ 0.93701096, -0.51345321, -0.33925233,  1.50418325],
-       [ 0.68642338, -1.44414332,  0.59936675, -0.94906993],
-       [-0.21398832,  0.14605547, -0.98294605, -0.46458507],
-       [ 0.18650391, -0.63992111, -1.06668243,  0.75731341]])
-
-w2 = np.array([[ 1.51446528, -0.3576823 ,  1.76784614],
-       [-1.12115099,  1.19909128, -0.6687727 ],
-       [ 2.26649579,  0.35408288, -0.02292686],
-       [ 0.04199896,  1.41332062,  1.09740199]])
-
-b1 = np.array([ 0.37678953, -0.58113617, -0.59270863, -0.9042974 ])
-
-b2 = np.array([-0.08474405,  1.27970495, -0.17627286])
+genotype = np.array([ 1.19456666,  0.79075852,  1.53501633, -0.09641386,  1.20048471,
+        0.70580301, -0.21671884, -1.88578059, -0.3363932 ])
 
 
-net = Network(w1, w2, b1, b2)
-robot = robot.Robot(max_motor_speed=1, lenght=0.15, wheelbase=0.2, sensor_n=8, sensor_width=0.07)
+net = Network(genotype, [8, 1])
+r = robot.Robot(max_motor_speed=1, lenght=0.15, wheelbase=0.2, sensor_n=8, sensor_width=0.07, position=[0, -0.1], engine_cutoff=0.1)
 
 t = Track()
 t.add_segment("straight")
-t.add_segment("90_left")
-t.add_segment("straight")
-t.add_segment("x-intersection")
-t.add_segment("straight")
-t.add_segment("90_right")
-t.add_segment("straight")
-for i in range(31):
+for i in range(35):
     t.add_segment("")
 
 clock = pygame.time.Clock()
@@ -64,14 +41,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    inputs = np.array(robot.get_sensors(t))
+    inputs = np.array(r.get_sensors(t))
     e1, e2 = net.forward(inputs)
-    s += robot.move(e1, e2, 1/100)
+    s += r.move(e1, e2, 1/100)
 
-    travelled_distance += robot.move(e1, e2, 1/100)
     # print(travelled_distance)
     ge.draw_track(t)
-    robot.draw(ge)
+    r.draw(ge)
     pygame.display.flip()
     clock.tick(60)
 
